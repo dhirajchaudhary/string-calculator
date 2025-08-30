a4d518ff44eeb48e4d624da3fbb19c930271886d
Dhiraj Singh <dhirajchaudhary33@gmail.com>
2025-08-31 02:10:35 +0530

    Added custom delimiter support and test for //;\n1;2

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,29 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers == ""
+
     delimiters = [',', "\n"]
-    nums = numbers.split(Regexp.union(delimiters)).map(&:to_i)
+    numbers_section = numbers
+
+    if numbers.start_with?('//')
+      header, numbers_section = numbers.split("\n", 2)
+      delimiter_part = header[2..-1]
+      # support [delim] style (multi-char) OR single-char delimiter
+      if delimiter_part.start_with?('[')
+        delimiters += delimiter_part.scan(/\[([^\]]+)\]/).flatten
+      else
+        delimiters << delimiter_part
+      end
+    end
+
+    regex = Regexp.union(delimiters)
+    nums = numbers_section.split(regex).map(&:strip).reject(&:empty?).map(&:to_i)
+
+    negatives = nums.select { |n| n < 0 }
+    unless negatives.empty?
+      raise ArgumentError, "negative numbers not allowed: #{negatives.join(',')}"
+    end
+
     nums.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,3 +1,27 @@
-it 'sums two comma separated numbers' do
-  expect(calc.add("1,5")).to eq(6)
+require_relative '../lib/string_calculator'
+
+RSpec.describe StringCalculator do
+  subject(:calc) { described_class.new }
+
+  describe '#add' do
+    it 'returns 0 for empty string' do
+      expect(calc.add("")).to eq(0)
+    end
+
+    it 'returns the number for a single number string' do
+      expect(calc.add("1")).to eq(1)
+    end
+
+    it 'sums two comma separated numbers' do
+      expect(calc.add("1,5")).to eq(6)
+    end
+
+		it 'handles newline as delimiter' do
+			expect(calc.add("1\n2,3")).to eq(6)
+		end
+
+		it 'supports custom single-character delimiter declared like //;\n1;2' do
+			expect(calc.add("//;\n1;2")).to eq(3)
+		end
+  end
 end
