c24dd839d3accf21348315a68484ecda217f5f0d
Dhiraj Singh <dhirajchaudhary33@gmail.com>
2025-08-31 02:14:09 +0530

    added remaining spec

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -23,5 +23,9 @@ RSpec.describe StringCalculator do
 		it 'supports custom single-character delimiter declared like //;\n1;2' do
 			expect(calc.add("//;\n1;2")).to eq(3)
 		end
+
+		it 'raises with all negative numbers listed' do
+			expect { calc.add("1,-2,3,-4") }.to raise_error(ArgumentError, "negative numbers not allowed: -2,-4")
+		end
   end
 end
